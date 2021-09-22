8ec2889ace5428f0a64864b1311b71778d86efe9
zanhung <zanhung@originlab.com>
2021-09-22 10:05:27 +0800

    update sscal(...)

--- src/sscal.rs
+++ src/sscal.rs
@@ -1,15 +1,10 @@
-use std::arch::x86_64::*;
-
-#[target_feature(enable="avx")]
-pub unsafe fn sscal(n: usize, a: f32, x: &mut [f32], incx: usize) -> bool {
-    if n == 0 {
-        return true;
-    }
-
-    if incx == 0 {
+/// Scales a vector by a constant.
+pub fn sscal(n: isize, a: f32, x: &mut [f32], incx: isize) -> bool {
+    if n <= 0 || incx <= 0 {
         return false;
     }
-    if x.len() < 1 + (n - 1) * incx {
+
+    if x.len() < 1 + ((n as usize) - 1) * (incx as usize) {
         return false;
     }
 
@@ -17,24 +12,13 @@ pub unsafe fn sscal(n: usize, a: f32, x: &mut [f32], incx: usize) -> bool {
         return true;
     }
 
-    let mut offset_x: usize = 0;
+    let mut ix: usize = 0;
 
-    let mut n_8 = 0_usize;
-    if std::is_x86_feature_detected!("avx") {
-        n_8 = n / 8;
-        let a_packed: __m256 = _mm256_set1_ps(a);
-        for _ in 0..n_8 {
-            let x_packed: __m256 = _mm256_set_slice_ps!(x[offset_x ..], incx);
-            let ax: __m256 = _mm256_mul_ps(a_packed, x_packed);
-            _mm256_get_ps!(x[offset_x ..], incx, ax);
-
-            offset_x += 8;
-        }
-    }
-
-    for _ in (n_8 * 8) .. n {
-        x[offset_x] *= a;
-        offset_x += incx;
+    let n_usize = n as usize;
+    let incx_usize = incx as usize;
+    for _ in 0 .. n_usize {
+        x[ix] *= a;
+        ix += incx_usize;
     }
 
     return true;
